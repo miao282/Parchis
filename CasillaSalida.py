fb24c2bda00968800ea3d2b45fd227227efb2fd2
moran368404 <moran368404@uabc.edu.mx>
2025-04-29 20:53:14 +0200

    CasillaSalida terminada con comentarios

---
+++ CasillaSalida.py
@@ -0,0 +1,7 @@
+class CasillaSalida(Casilla):
+    def __init__(self, posicion: int):
+        super().__init__(posicion)
+        self.esta_activada = False
+
+    def activar(self):
+        self.esta_activada = True
