054d91ceb5527f4afb685cd1fb16fae898f75162
moran368404 <moran368404@uabc.edu.mx>
2025-04-29 20:52:15 +0200

    Clase CasillaMeta con descripcion de clase, tendremos mas de una casilla meta (diferentes casillas meta con diferente posicion)? o solamente sera una (un conjunto de casillas que contendria 4 posiciones)?

---
+++ CasillaMeta.py
@@ -0,0 +1,14 @@
+class CasillaMeta(Casilla):
+    """""""""
+    Esta casilla es una de las herencias de Casilla. Esta clase representa una de las metas de los
+    jugadores, cuando los jugadores lleguen a esta casilla signica que estan en una zona segura
+    a la que ningun otro jugador puede acudir, cuando tenga 4 fichas dentro de la casilla meta
+    significa que ha sido el ganador.
+    """""""""
+    def __init__(self, posicion: int, jugador: Jugador):
+        super().__init__(posicion)
+        self.llegadas: List['Ficha'] = []
+        self.jugador = jugador
+
+    def registrar_llegada(self, ficha: 'Ficha'):
+        self.llegadas.append(ficha)
